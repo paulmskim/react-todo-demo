df7aa4e38f946b661d6d47c8ec010da455c108fa
paulmskim <kim.paul.ms@gmail.com>
2017-02-07 17:29:41 +0800

    create filtered todo list and add to app

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import AddTodo from './AddTodo';
+import FilteredTodoList from './FilteredTodoList';
 
 const App = () => (
   <div>
     <AddTodo />
+    <FilteredTodoList />
   </div>
 );
 
