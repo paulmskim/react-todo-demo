9ae1087df7b5202d952cf4ec8da4b5ddcdba2a97
paulmskim <kim.paul.ms@gmail.com>
2017-02-07 18:09:20 +0800

    add footer with filter links and add to app

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AddTodo from './AddTodo';
 import FilteredTodoList from './FilteredTodoList';
+import Footer from './Footer';
 
 const App = () => (
   <div>
     <AddTodo />
     <FilteredTodoList />
+    <Footer />
   </div>
 );
 
