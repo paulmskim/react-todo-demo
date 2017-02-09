43693cb88e08a6e78af9650696944cd986613c59
paulmskim <kim.paul.ms@gmail.com>
2017-02-09 11:27:46 +0800

    add test file and cover todo reducer

---
+++ test/reducers/todo.spec.js
@@ -0,0 +1,125 @@
+import { expect } from 'chai';
+import deepFreeze from 'deep-freeze';
+import todo from '../../src/js/reducers/todo';
+
+describe('add todo action', () => {
+  it('should add todo', () => {
+    const stateBefore = {};
+    const action = {
+      type: 'ADD_TODO',
+      id: 0,
+      text: 'Hello world'
+    };
+    const stateAfter = {
+      id: 0,
+      text: 'Hello world',
+      completed: false
+    };
+
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+
+    expect(
+      todo(stateBefore, action)
+    ).to.deep.equal(stateAfter);
+  });
+});
+
+describe('toggle todo action', () => {
+  it('should toggle completed field if state id matches action id', () => {
+    const stateBefore = {
+      id: 0,
+      text: 'Hello world',
+      completed: false
+    };
+    const action = {
+      type: 'TOGGLE_TODO',
+      id: 0
+    };
+    const stateAfter = {
+      id: 0,
+      text: 'Hello world',
+      completed: true
+    };
+
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+
+    expect(
+      todo(stateBefore, action)
+    ).to.deep.equal(stateAfter);
+  });
+
+  it('should not toggle completed field if state id does not matches action id', () => {
+    const stateBefore = {
+      id: 0,
+      text: 'Hello world',
+      completed: false
+    };
+    const action = {
+      type: 'TOGGLE_TODO',
+      id: 1
+    };
+    const stateAfter = {
+      id: 0,
+      text: 'Hello world',
+      completed: false
+    };
+
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+
+    expect(
+      todo(stateBefore, action)
+    ).to.deep.equal(stateAfter);
+  });
+});
+
+describe('default action', () => {
+  it('should return state when an unknown action is defined', () => {
+    const stateBefore = {
+      id: 0,
+      text: 'Hello world',
+      completed: false
+    };
+    const action = {
+      action: 'DEFAULT_ACTION',
+      id: 0
+    };
+    const stateAfter = {
+      id: 0,
+      text: 'Hello world',
+      completed: false
+    };
+
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+
+    expect(
+      todo(stateBefore, action)
+    ).to.deep.equal(stateAfter);
+  });
+
+  it('should return state when no action is defined', () => {
+    const stateBefore = {
+      id: 0,
+      text: 'Hello world',
+      completed: false
+    };
+    const action = {
+      id: 0
+    };
+    const stateAfter = {
+      id: 0,
+      text: 'Hello world',
+      completed: false
+    };
+
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+
+    expect(
+      todo(stateBefore, action)
+    ).to.deep.equal(stateAfter);
+  });
+});
